a74eabff42f439c633323ea22f4f1f82a00c07e2
Martin Broesamle <m@m-broe.de>
2016-08-05 19:35:59 +0200

    Use extern library opengl_graphics.

--- minimals/graphout/src/main.rs
+++ minimals/graphout/src/main.rs
@@ -1,13 +1,18 @@
 extern crate piston;
 extern crate glutin_window;
+extern crate opengl_graphics;
 
 use glutin_window::GlutinWindow as Window;
 use piston::window::WindowSettings;
+use opengl_graphics::{GlGraphics, OpenGL};
 
 fn main() {
     let window: Window =
         WindowSettings::new("Hello World!", [512; 2])
             .build().unwrap();
+
+    let opengl = OpenGL::V3_2;
+    let gl = GlGraphics::new(opengl);
 
     loop { }
 }
