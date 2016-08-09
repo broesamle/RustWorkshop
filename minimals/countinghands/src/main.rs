d62746a97b06b7042ec74fba50df99e75cd8eb79
Martin Broesamle <m@m-broe.de>
2016-08-09 21:27:47 +0200

    Attempt to assign new value to immutable var.

--- minimals/countinghands/src/main.rs
+++ minimals/countinghands/src/main.rs
@@ -2,7 +2,11 @@ fn main() {
     let apples = 8;
     let fingers = 3;
     let peanuts = 2;
-    println!("Hello, world!");
+
     println!("There are {} apples, we see {} fingers and how many peanuts? {}, exactly.",
         apples, fingers, peanuts);
+    apples = 12;
+    println!("Now, there are {} apples, we see {} fingers and how many peanuts? {}, exactly.",
+        apples, fingers, peanuts);
+
 }
