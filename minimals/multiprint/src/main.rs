99eeef5e603b5e8cf5c6f415dfb6972cd46fcdd5
Martin Broesamle <m@m-broe.de>
2016-08-22 16:32:33 +0200

    Print messages after spawning new threads.

--- minimals/multiprint/src/main.rs
+++ minimals/multiprint/src/main.rs
@@ -3,5 +3,6 @@ use std::thread;
 fn main() {
     for num in 0..10 {
         thread::spawn(move || { println!("Thread {}", num); });
+        println!("Started thread number {:?}.", num);
     }
 }
