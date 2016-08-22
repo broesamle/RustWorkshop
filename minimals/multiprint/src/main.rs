7a9fcaf4b1db87160caf5cb80f919d8f53bdc7c2
Martin Broesamle <m@m-broe.de>
2016-08-22 17:38:02 +0200

    Collect and print the result of the joining.

--- minimals/multiprint/src/main.rs
+++ minimals/multiprint/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     println!("Vector of {} join handles.", threads.len());
     for num in (0..10).rev() {
         let thr = threads.remove(num);
-        thr.join();
-        println!("Joined thread number {:?}.", num);
+        let joinresult = thr.join();
+        println!("Joined thread number {:?}, {:?}.", num, joinresult);
     }
 }
