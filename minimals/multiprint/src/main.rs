342c98a32508069ee8d3a28bb277c0b0e39f6531
Martin Broesamle <m@m-broe.de>
2016-08-22 16:52:49 +0200

    Collecting join handles in vector.

--- minimals/multiprint/src/main.rs
+++ minimals/multiprint/src/main.rs
@@ -1,8 +1,11 @@
 use std::thread;
 
 fn main() {
+    let mut threads = Vec::new();
     for num in 0..10 {
-        thread::spawn(move || { println!("Thread {}", num); });
+        let handle = thread::spawn(move || { println!("Thread {}", num); });
+        threads.push(handle);
         println!("Started thread number {:?}.", num);
     }
+    println!("Vector of {} join handles.", threads.len());
 }
